8f1e6adea36f680db5072d49476a80e9ddff8202
RahmaLindungiLaowo <rahmalindungilaowo@gmail.com>
2025-08-28 22:02:46 +0700

    practice array is done

--- src/array/practice/Practice9.java
+++ src/array/practice/Practice9.java
@@ -9,16 +9,46 @@ public class Practice9 {
         String[] productsName = new String[10];
         int[] productsPrice = new int[10];
 
-        int productsCount;
+        int productsCount = 0;
 
-        System.out.println("[ 1. Add product  |  2. List product  |  3. Exit ]");
-
-        System.out.println("Please select menu: ");
-        int choice = scanner.nextInt();
+        int[] choices = {1, 2, 3};
 
         while(true){
-            if(choice == 1){
+            System.out.println("[ 1. Add product  |  2. List product  |  3. Exit ]");
 
+            System.out.print("Please select menu: ");
+            int choice = scanner.nextInt();
+            int[] maxProducts = new int[4];
+
+            if (choice == choices[0]) {
+                System.out.print("- Enter the a product name: ");
+                productsName[productsCount] = scanner.next();
+                System.out.print("- Enter the a product price: ");
+                productsPrice[productsCount] = scanner.nextInt();
+                productsCount++;
+                if (productsCount == maxProducts.length) {
+                    System.out.println("- You can no longer add products.");
+                    break;
+                }
+            }
+
+            if (choice == choices[1]){
+                if (productsCount == 0) {
+                    System.out.println("- There are no products listed.");
+                    break;
+                }
+                System.out.println("List of products:");
+                for (int i = 0; i < productsCount; i++) {
+                    System.out.println("- " + productsName[i] + " : " + productsPrice[i] + " rupiah");
+                }
+            }
+            if (choice == choices[2]){
+                System.out.println("- Exit the program.");
+                break;
+            }
+            if (choice > choices.length){
+                System.out.println("- You selected the wrong menu.");
+                break;
             }
         }
     }
