fbcde67b94050f03fa2c0935c73c229064642314
RahmaLindungiLaowo <rahmalindungilaowo@gmail.com>
2025-08-29 23:28:57 +0700

    practice methods

--- src/method/practice/Practice4.java
+++ src/method/practice/Practice4.java
@@ -3,56 +3,55 @@ package method.practice;
 import java.util.Scanner;
 
 public class Practice4 {
+    static int balances = 0;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         while (true) {
             System.out.println("------------------------------------------------------");
             System.out.println("1. Deposit | 2. Withdraw | 3. Check Balance | 4. Exit ");
             System.out.println("------------------------------------------------------");
             System.out.print("Select: ");
             int choice = scanner.nextInt();
-            int balance = 0;
             int depositAmount = 0;
             switch (choice) {
                 case 1:
-                    balance = deposit(balance, depositAmount);
+                    balances += deposit(balances, depositAmount);
                     break;
                 case 2:
-                    withdraw(balance);
+                    balances -= withdraw(balances);
                     break;
                 case 3:
-                    checkBalance();
+                    balances = checkBalance(balances);
                     break;
-                case 4:
-                    System.out.println("Thank you for using our service.");
-                    break;
-                default:
-                    System.out.println("Invalid choice. Please try again.");
-                    break;
+            }
+            if (choice == 4) {
+                System.out.println("Exit the program.");
+                break;
             }
         }
     }
-    public static int deposit(int balance, int depositAmount){
+    public static int deposit(int balances, int depositAmount){
         Scanner scannerDeposit = new Scanner(System.in);
         System.out.print("Enter the deposit amount: ");
         int deposit = scannerDeposit.nextInt();
-        balance += deposit;
-        System.out.println("You have deposited " + deposit + " rupiah. Current balance = " + balance + " rupiah.");
+        balances += deposit;
+        System.out.println("You have deposited " + deposit + " rupiah. Current balance = " + balances + " rupiah.");
         return deposit;
     }
-    public static void withdraw(int balance){
+    public static int withdraw(int balances) {
         Scanner scannerWithdraw = new Scanner(System.in);
-        System.out.println("Enter the withdrawal amount: ");
+        System.out.print("Enter the withdrawal amount: ");
         int withdraw = scannerWithdraw.nextInt();
-        if (balance >= withdraw){
-            balance -= withdraw;
-        }else {
-            System.out.println("Your balance is minus.");
+        if (balances >= withdraw) {
+            balances -= withdraw;
+        } else {
+            System.out.println("Your tried to withdraw " + withdraw + " rupiah, but your balance is insufficient.");
         }
-        System.out.println("You tried to withdraw " + withdraw + " rupiah. Current balance: " + balance + " rupiah.");
+        System.out.println("You have withdraw " + withdraw + " rupiah. Current balance: " + balances + " rupiah.");
+        return withdraw;
     }
-    public static void checkBalance(){
+    public static int checkBalance(int balances){
+        System.out.println("Your balance is " + balances + " rupiah.");
+        return balances;
     }
-
 }
