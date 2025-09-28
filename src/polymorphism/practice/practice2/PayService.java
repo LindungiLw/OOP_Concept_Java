71d5592c903cdb238b14fd6bb1436886fe0b5ef8
RahmaLindungiLaowo <rahmalindungilaowo@gmail.com>
2025-09-28 23:21:36 +0700

    practice2

---
+++ src/polymorphism/practice/practice2/PayService.java
@@ -0,0 +1,23 @@
+package polymorphism.practice.practice2;
+
+public class PayService {
+    public void processPay(String option, int amount) {
+        boolean result;
+        System.out.println("Start payment: option=" + option + ", amount=" + amount);
+        if (option.equals("go")) {
+            GoPay goPay = new GoPay();
+            result = goPay.pay(amount);
+        } else if (option.equals("ovo")) {
+            OvoPay ovoPay = new OvoPay();
+            result = ovoPay.pay(amount);
+        } else {
+            System.out.println("There is no payment method.");
+            result = false;
+        }
+        if (result) {
+            System.out.println("The Payment was successful.");
+        } else {
+            System.out.println("The Payment was failed.");
+        }
+    }
+}
